9c3f5dfdd81da171cfcfe99aa2c285134380bdcc
Alex Reyes <alex.reyesn96@gmail.com>
2025-05-19 19:44:13 -0500

    update properties

--- Module.Products.Shared/DBModels/EntityProduct.cs
+++ Module.Products.Shared/DBModels/EntityProduct.cs
@@ -5,5 +5,5 @@ public class EntityProduct
     public Guid Id { get; set; } = new Guid();
     public string Name { get; set; }
     public decimal Price { get; set; }  
-    public string AdditionalPropertyLocalUpdateFromRemotexxx12345 { get; set; }
+    public string AdditionalProperty { get; set; }
 }
