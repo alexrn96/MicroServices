cad17140be52e6f1605299f8a3443e55a96e39a2
alexrn96 <alex.reyesn96@gmail.com>
2025-05-19 19:49:37 -0500

    Update ProductDto.cs

--- Module.Products.Shared/DataTransferObjects/ProductDto.cs
+++ Module.Products.Shared/DataTransferObjects/ProductDto.cs
@@ -5,5 +5,5 @@ public class ProductDto
 {
     public string Name { get; set; }
     public decimal Price { get; set; }
-    public string AdditionalProperty { get; set; }
+    public string UpdatedDtoProperty { get; set; }
 }
