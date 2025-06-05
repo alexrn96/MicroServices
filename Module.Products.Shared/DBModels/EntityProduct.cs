d113cafcedde0134d4bb627ef1d26bdad507f4eb
Alex Reyes <alex.reyesn96@gmail.com>
2025-06-04 22:17:32 -0500

    remove unnecesary properties from Dtos

--- Module.Products.Shared/DBModels/EntityProduct.cs
+++ Module.Products.Shared/DBModels/EntityProduct.cs
@@ -4,6 +4,5 @@ public class EntityProduct
 {
     public Guid Id { get; set; } = new Guid();
     public string Name { get; set; }
-    public decimal Price { get; set; }  
-    public string AdditionalPropertyLocalUpdateFromRemotexxx { get; set; }
+    public decimal Price { get; set; }
 }
--- Module.Products.Shared/DataTransferObjects/ProductDto.cs
+++ Module.Products.Shared/DataTransferObjects/ProductDto.cs
@@ -5,5 +5,4 @@ public class ProductDto
 {
     public string Name { get; set; }
     public decimal Price { get; set; }
-    public string AdditionalPropertyLocalDtoUpdatedFromRemotexxx { get; set; }
 }
